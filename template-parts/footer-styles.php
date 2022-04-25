721d225537e32447dca9f067dddcaf63785633db
Antoni-Mestre <62470124+Antoni-Mestre@users.noreply.github.com>
2022-04-26 00:51:56 +0200

    Add files via upload

---
+++ template-parts/footer-styles.php
@@ -0,0 +1,45 @@
+<!-- footer styles -->
+
+<style> .u-footer {
+  background-image: none;
+}
+.u-footer .u-sheet-1 {
+  min-height: 22px;
+}
+.u-footer .u-text-1 {
+  width: 417px;
+  margin: 0 auto;
+}
+@media (max-width: 1199px) {
+  .u-footer .u-sheet-1 {
+    min-height: 23px;
+  }
+  .u-footer .u-text-1 {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+}
+@media (max-width: 991px) {
+  .u-footer .u-sheet-1 {
+    min-height: 24px;
+  }
+  .u-footer .u-text-1 {
+    margin-top: 1px;
+    margin-bottom: 1px;
+  }
+}
+@media (max-width: 767px) {
+  .u-footer .u-sheet-1 {
+    min-height: 22px;
+  }
+  .u-footer .u-text-1 {
+    margin-top: 0;
+  }
+}
+@media (max-width: 575px) {
+  .u-footer .u-text-1 {
+    width: auto;
+    margin-left: 0;
+    margin-right: 0;
+  }
+}</style>
---
+++ template-parts/header-styles.php
@@ -0,0 +1,34 @@
+<!-- header styles -->
+
+<?php
+   $localFonts = apply_filters('get_local_fonts', '');
+?>
+<?php if ($localFonts) : ?> 
+   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/<?php echo $localFonts; ?>" media="screen" type="text/css" />
+<?php else : ?>
+   <?php endif; ?>
+<link id="u-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+<style> .u-header {
+  background-image: none;
+}
+.u-header .u-sheet-1 {
+  min-height: 66px;
+}
+.u-header .u-menu-1 {
+  margin: 15px 0 15px auto;
+}
+.u-header .u-nav-1 {
+  font-size: 1rem;
+}
+.u-header .u-nav-2 {
+  font-size: 1.25rem;
+}
+@media (max-width: 1199px) {
+  .u-header .u-menu-1 {
+    width: auto;
+  }
+  .u-header .u-nav-1 {
+    letter-spacing: normal;
+    font-weight: 400;
+  }
+}</style>
